3b8ade134b64bf89831e232e3d9a20230a45dd9b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 13:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	let string1=s1[0];
+	let string2=s2[0];
+	if(string1===" " || string2===" "){
+		return " ";
+	}
+	let result=string1+string2;
+	return result;
+	
 }
 
 // Do not change the code below.
